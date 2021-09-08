0b1a0cf1d57aede3abd3203e7fa6a5b382a53de9
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-09-08 23:00:14 +0530

    refactored code

--- Main.java
+++ Main.java
@@ -3,18 +3,15 @@ package generics;
 public class Main {
 	
 	public static void main(String args[]) {
-		String a1="Amazon", b1 = "Tiger", c1 = "Zebra";
-		TestMaximum obj1 = new TestMaximum();
-		obj1.initString(a1, b1, c1);
-		obj1.findStringMax(a1, b1, c1);;
-		String a2 = "Zebra",b2="Amazon",c2="Tiger";
-		TestMaximum obj2 = new TestMaximum();
-		obj1.initString(a2, b2, c2);
-		obj2.findStringMax(a2, b2, c2);;
-		String a3="Tiger",b3="Zebra",c3="Amazon";
-		TestMaximum obj3 = new TestMaximum();
-		obj1.initString(a3, b3, c3);
-		obj3.findStringMax(a3, b3, c3);;
+		String a1="Amazon", b1 = "P", c1 = "Banana";
+		TestMaximum<String> obj1 = new TestMaximum<>(a1, b1, c1);
+		obj1.findMax();
+		Float a2 =32.2f, b2 = 70.3f, c2 = 84.6f;
+		TestMaximum<Float> obj2=new TestMaximum<>(a2, b2, c2);
+		obj2.findMax();
+		Integer a3 = 43, b3 = 67, c3 = 89;
+		TestMaximum<Integer> obj3=new TestMaximum<>(a3, b3, c3);
+		obj3.findMax();
 	}
 
 }
