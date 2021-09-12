3a435cc0d60ec4455f9449b2af9170864d6ffab4
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-09-12 19:57:08 +0530

    more than 3 params can be passed

--- TestMaximum.java
+++ TestMaximum.java
@@ -2,38 +2,47 @@ package generics;
 
 public class TestMaximum<T extends Comparable<T>> {
 	
-	 public  T a,b,c;
+	 public  T a,b,c,d;
 	
 	
 	  
-	  public TestMaximum(T a, T b, T c) {
-		  this.a = a;
-		  this.b = b;
-		  this.c = c;
+	  public TestMaximum(T a,T b,T c) {
+		  this.a=a;
+		  this.b=b;
+		  this.c=c;
+	  }
+	  public TestMaximum(T a,T b,T c,T d) {
+		  this.a=a;
+		  this.b=b;
+		  this.c=c;
+		  this.d=d;
 	  }
 	
 	
-
-	  
-	  public void findMax() {
-		  T res = TestMaximum.testMaximum(a, b, c);
-		  System.out.println("Maximum of " + a + ", " + b + " and " + c + " is = " + res);
-	  }
 	
-	public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
-		
-		T max = a;
-		
-		if (b.compareTo(max) > 0) {
+	public static <T extends Comparable<T>> void testMaximum(T ...values) {
 
-			max = b;
+		T temp;
+		int len=values.length;
+		
+		for(int i=0;i<len-1;i++) {
+			for(int j=0;j<len-i-1;j++) {
+				if(values[j].compareTo(values[j+1]) >0) {
+					temp=values[j];
+					values[j]=values[j+1];
+					values[j+1]=temp;
+					
+				}
+			}
 		}
-		if (c.compareTo(max) > 0) {
+		System.out.print("Maximum of ");
+		for(T t : values) {
+			System.out.print(t+", ");
 			
-			max = c;
 		}
+		System.out.println("is "+values[values.length-1]);		
 
-		return max;
-
-	}
+	
 }
+}
+	
